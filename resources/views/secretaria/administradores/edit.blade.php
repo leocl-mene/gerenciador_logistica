b1664ef89a0625e8ce71fb468a25dfd6a12f517c
Leonardo Chaves Lima <leonardo.chaves@meneportella.com.br>
2026-01-09 15:15:27 -0400

    Exibe erros de validacao em administradores

--- resources/views/secretaria/administradores/edit.blade.php
+++ resources/views/secretaria/administradores/edit.blade.php
@@ -9,6 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
+                    @if ($errors->any())
+                        <div class="mb-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
+                            <ul class="list-disc ps-5 text-sm">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
                     <form method="POST" action="{{ route('administradores.update', $administrador->id) }}">
                         @csrf
                         @method('PUT')
