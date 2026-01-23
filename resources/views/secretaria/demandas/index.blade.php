c610d033e07fd9c527f0250e0750c303b955abb8
Leonardo Chaves Lima <leonardo.chaves@meneportella.com.br>
2026-01-23 11:56:43 -0400

    Ajusta rolagem horizontal da tabela de demandas

--- resources/views/secretaria/demandas/index.blade.php
+++ resources/views/secretaria/demandas/index.blade.php
@@ -23,8 +23,9 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
 
                     {{-- Lista de Demandas (somente leitura) --}}
-                    <div class="overflow-x-auto">
-                        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <div class="overflow-x-auto -mx-6">
+                        <div class="min-w-[1100px] px-6">
+                            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                             <thead class="bg-gray-50 dark:bg-gray-700">
                                 <tr>
                                     <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
@@ -140,7 +141,8 @@
                                     </tr>
                                 @endforelse
                             </tbody>
-                        </table>
+                            </table>
+                        </div>
                     </div>
 
                 </div>
